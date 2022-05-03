58aca7b91368196b18fa0c9a148fd08aced2b698
Román Martínez <rgmf@riseup.net>
2022-05-03 12:54:50 +0200

    Tras primera clase.

--- Coche/src/es/rgmf/Main.java
+++ Coche/src/es/rgmf/Main.java
@@ -2,5 +2,12 @@ package es.rgmf;
 
 public class Main {
     public static void main(String[] args) {
+        Coche ford = new Coche("Ford", "Focus", "rojo");
+
+        System.out.println(ford);
+
+        ford.incrementarVelocidad(5);
+
+        System.out.println(ford);
     }
 }
